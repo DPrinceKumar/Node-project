f0818f426b879b443ed4ff340e01b7fd1ac6286a
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-17 19:00:07 +0530

    added multiple records in data.js

--- data.js
+++ data.js
@@ -4,6 +4,221 @@ const data = [
     name: "name of deom 1",
     phone: 000,
   },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
+  {
+    id: "deom 1",
+    name: "name of deom 1",
+    phone: 000,
+  },
 ];
 module.exports = {
   data,

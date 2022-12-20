77646342a75b64abaac41a8497819d4e9bc5f46e
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-20 11:32:45 +0530

    added json data, inclued static file in node,some styling

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express=require('express');
 const data=require('./data');
+const htmlFile=__dirname;
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
+
+app.use(express.static(htmlFile));
+app.get("/", (req, res) => {
+    // res.send(
+    //   "<h1 style='font-family: Segoe UI', Tahoma, Geneva, Verdana, sans-serif;'> Please type '/useless' in search bar </h1>"
+    // );
+//   res.send(index);
+res.sendFile(htmlFile+"/index.html");
+});
 
 app.get('/useless',(_,resp)=>{
     resp.send(data);
 });
 
-app.listen(port,(port)=>{
+app.listen(port,()=>{
     console.log("listing to the PORT "+ port)
     console.log("The value of PORT is:", process.env.PORT);
 });
